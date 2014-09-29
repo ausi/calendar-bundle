6c13b426b008568a56511d04ac50af17ef374c46
Leo Feyer <github@contao.org>
2014-09-29 11:55:16 +0200

    Port the changes from contao/core@fb3e554149547530c6bc62b80b13c502cdb7f88c

--- src/dca/tl_calendar_events.php
+++ src/dca/tl_calendar_events.php
@@ -409,7 +409,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events'] =
 			'label'                   => &$GLOBALS['TL_LANG']['tl_calendar_events']['enclosure'],
 			'exclude'                 => true,
 			'inputType'               => 'fileTree',
-			'eval'                    => ['multiple'=>true, 'fieldType'=>'checkbox', 'filesOnly'=>true, 'mandatory'=>true],
+			'eval'                    => ['multiple'=>true, 'fieldType'=>'checkbox', 'filesOnly'=>true, 'isDownloads'=>true, 'extensions'=>Config::get('allowedDownload'), 'mandatory'=>true],
 			'sql'                     => "blob NULL"
 		],
 		'source' =>

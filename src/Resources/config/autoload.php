c0bb23206e37afceae4518a574330daf1592513f
Leo Feyer <github@contao.org>
2014-10-05 20:14:47 +0200

    Fix the `autoload.php` file

--- src/Resources/config/autoload.php
+++ src/Resources/config/autoload.php
@@ -13,15 +13,15 @@
 // Templates
 TemplateLoader::addFiles(
 [
-	'cal_default'        => 'vendor/contao/modules-calendar/src/Resources/templates/calendar',
-	'cal_mini'           => 'vendor/contao/modules-calendar/src/Resources/templates/calendar',
-	'event_full'         => 'vendor/contao/modules-calendar/src/Resources/templates/events',
-	'event_list'         => 'vendor/contao/modules-calendar/src/Resources/templates/events',
-	'event_teaser'       => 'vendor/contao/modules-calendar/src/Resources/templates/events',
-	'event_upcoming'     => 'vendor/contao/modules-calendar/src/Resources/templates/events',
-	'mod_calendar'       => 'vendor/contao/modules-calendar/src/Resources/templates/modules',
-	'mod_event'          => 'vendor/contao/modules-calendar/src/Resources/templates/modules',
-	'mod_eventlist'      => 'vendor/contao/modules-calendar/src/Resources/templates/modules',
-	'mod_eventmenu'      => 'vendor/contao/modules-calendar/src/Resources/templates/modules',
-	'mod_eventmenu_year' => 'vendor/contao/modules-calendar/src/Resources/templates/modules',
+	'cal_default'        => 'vendor/contao/module-calendar/src/Resources/templates/calendar',
+	'cal_mini'           => 'vendor/contao/module-calendar/src/Resources/templates/calendar',
+	'event_full'         => 'vendor/contao/module-calendar/src/Resources/templates/events',
+	'event_list'         => 'vendor/contao/module-calendar/src/Resources/templates/events',
+	'event_teaser'       => 'vendor/contao/module-calendar/src/Resources/templates/events',
+	'event_upcoming'     => 'vendor/contao/module-calendar/src/Resources/templates/events',
+	'mod_calendar'       => 'vendor/contao/module-calendar/src/Resources/templates/modules',
+	'mod_event'          => 'vendor/contao/module-calendar/src/Resources/templates/modules',
+	'mod_eventlist'      => 'vendor/contao/module-calendar/src/Resources/templates/modules',
+	'mod_eventmenu'      => 'vendor/contao/module-calendar/src/Resources/templates/modules',
+	'mod_eventmenu_year' => 'vendor/contao/module-calendar/src/Resources/templates/modules',
 ]);

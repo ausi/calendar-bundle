2f25d8da74f6251daeab8cdae92314cb9659f68d
Leo Feyer <github@contao.org>
2014-10-29 21:15:18 +0100

    Define the public folders in the bundle class

--- src/ContaoCalendarBundle.php
+++ src/ContaoCalendarBundle.php
@@ -5,14 +5,27 @@
  *
  * Copyright (c) 2005-2014 Leo Feyer
  *
- * @link    https://contao.org
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * @license LGPL-3.0+
  */
 
 namespace Contao\CalendarBundle;
 
 use Contao\CoreBundle\HttpKernel\Bundle\ContaoBundle;
 
+/**
+ * Configures the Contao calendar bundle.
+ *
+ * @author Leo Feyer <https://contao.org>
+ */
 class ContaoCalendarBundle extends ContaoBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicFolders()
+    {
+        return [
+            $this->getPath() . '/../contao/assets'
+        ];
+    }
 }

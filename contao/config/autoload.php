f13f934c04e4dc5f40e6a0b5b7e40c2e0b298490
Leo Feyer <github@contao.org>
2014-10-29 16:29:40 +0100

    Add the ContaoBundle::getContaoResourcesPath() method

---
+++ contao/config/autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Calendar
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+// Templates
+TemplateLoader::addFiles(
+[
+	'cal_default'        => 'vendor/contao/module-calendar/contao/templates/calendar',
+	'cal_mini'           => 'vendor/contao/module-calendar/contao/templates/calendar',
+	'event_full'         => 'vendor/contao/module-calendar/contao/templates/events',
+	'event_list'         => 'vendor/contao/module-calendar/contao/templates/events',
+	'event_teaser'       => 'vendor/contao/module-calendar/contao/templates/events',
+	'event_upcoming'     => 'vendor/contao/module-calendar/contao/templates/events',
+	'mod_calendar'       => 'vendor/contao/module-calendar/contao/templates/modules',
+	'mod_event'          => 'vendor/contao/module-calendar/contao/templates/modules',
+	'mod_eventlist'      => 'vendor/contao/module-calendar/contao/templates/modules',
+	'mod_eventmenu'      => 'vendor/contao/module-calendar/contao/templates/modules',
+	'mod_eventmenu_year' => 'vendor/contao/module-calendar/contao/templates/modules',
+]);
